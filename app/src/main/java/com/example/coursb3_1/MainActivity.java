e6f5a5ba55fc02e0090fa288113c4031e869d500
Elouan <louloulfx1@hotmail.fr>
2020-05-12 08:41:09 +0200

    tp finish

--- app/src/main/java/com/example/coursb3_1/MainActivity.java
+++ app/src/main/java/com/example/coursb3_1/MainActivity.java
@@ -1,8 +1,18 @@
 package com.example.coursb3_1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && manager != null) {
+            NotificationChannel channel = new NotificationChannel("groupe1", "Nom du groupe", NotificationManager.IMPORTANCE_HIGH);
+            channel.setDescription("Description du groupe");
+            channel.enableLights(true);
+
+            channel.setLightColor(Color.RED);
+            channel.enableVibration(true);
+            channel.setVibrationPattern(new long[]{100, 200, 300});
+            manager.createNotificationChannel(channel);
+        }
+    }
+
+    public void OnClick(View view) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "groupe1")
+                .setSmallIcon(R.drawable.icone_notification)
+                .setContentTitle("Ma notification")
+                .setAutoCancel(true)
+                .setContentText("Bienvenue !");
+        Intent intent = new Intent(this, DetailActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(DetailActivity.class);
+        stackBuilder.addNextIntent(intent);
+        PendingIntent pendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(pendingIntent);
+
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (manager != null) {
+            manager.notify(1234, builder.build());
+        }
     }
 }
